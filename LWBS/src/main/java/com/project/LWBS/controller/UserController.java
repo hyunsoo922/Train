1e21ec2368fb1e583d9485c9d10502959d738538
skrheem <skrheem1116@gmail.com>
2023-11-19 21:18:00 +0900

    segyuController : 내가 쓸 컨트롤러 index.html : http://localhost:8093 접속 시 기본 페이지 home.html : http://localhost:8093/home 접속 시 페이지 header.html, nav.html : 모든 페이지에서 공통적으로 사용할 페이지 상단 부분과 네비게이션 바

--- LWBS/src/main/java/com/project/LWBS/controller/UserController.java
+++ LWBS/src/main/java/com/project/LWBS/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.project.LWBS.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
+
 public class UserController {
+    public static void main(String[] args) {
+        SpringApplication.run(com.project.LWBS.LwbsApplication.class, args);
+    }
 }
