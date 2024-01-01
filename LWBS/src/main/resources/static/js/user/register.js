0766acb5ea147e5de84466e5ce8a597171dfe6aa
hyunsoo922 <hskang9200@naver.com>
2024-01-01 14:40:18 +0900

    회원가입 시 학생 또는 서점에서 입력항목 미입력시 경고출력

--- LWBS/src/main/resources/static/js/user/register.js
+++ LWBS/src/main/resources/static/js/user/register.js
@@ -9,14 +9,40 @@ $(function () {
 
         $("." + selectKind + "-container").removeClass('hidden');
 
-        if(selectKind == "student")
-        {
+        if (selectKind == "student") {
             $("#publisherId").val("");
         }
-        else if(selectKind == "bookStore")
-        {
+        else if (selectKind == "bookStore") {
             $("#LMSID").val("");
             $("#LMSPW").val("");
         }
     });
+
+    $("#regBtn").click(function () {
+        const frm = $("#register");
+        const kind = $("input[name='kind']:checked").val();
+        if (kind === "student") {
+            const ID = $("#LMSID").val().trim();
+            const PW = $("#LMSPW").val().trim();
+            if (ID === "" || PW === "") {
+                alert("아이디와 비밀번호를 입력해주세요.");
+            }
+            else {
+                frm.submit();
+            }
+        }
+        else {
+            const publishID = $("#publisherId").val().trim();
+            if (publishID === "") {
+                alert("가맹점ID를 입력해주세요.")
+            }
+            else {
+                frm.submit();
+            }
+        }
+
+
+
+    });
+
 });
