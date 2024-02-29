6d069077641f1d6fd1c81c43d9be775eaf8bd453
yeongmin0410 <yeongmin0410@naver.com>
2024-02-29 21:47:11 +0900

    배너 CSS 폰트 사이즈 너비 높이 수정 배너 html 문구 수정 book html CSS 출력 방식 수정 결제 전 영수증 표로 만들고 배치 내 구매 목록 4*3 그리드로 만들고 배치 StudentController에서 내용이 없을 시 표시해야할 부분 수정

--- LWBS/src/main/java/com/project/LWBS/controller/StudentController.java
+++ LWBS/src/main/java/com/project/LWBS/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.project.LWBS.domain.Book;
 import com.project.LWBS.domain.Receipt;
 import com.project.LWBS.domain.Receive;
 import com.project.LWBS.domain.User;
+import com.project.LWBS.service.MyPageService;
 import com.project.LWBS.service.StudentService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +23,12 @@ import java.util.List;
 public class StudentController {
 
     private StudentService studentService;
+    private MyPageService myPageService;
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, MyPageService myPageService) {
         this.studentService = studentService;
+        this.myPageService = myPageService;
     }
 
     @GetMapping("/purchase/book")
@@ -33,6 +36,7 @@ public class StudentController {
     {
             User user = principalDetails.getUser();
             model.addAttribute("user",user);
+            int sum = myPageService.sumMileage(user.getId());
 
             List<Book> bookList = studentService.findMyClass(user);
 
@@ -42,6 +46,7 @@ public class StudentController {
 
             model.addAttribute("bookList", bookList);
             model.addAttribute("receiveDays",receiveList);
+            model.addAttribute("sumpoint", sum);
 
     }
 
@@ -100,33 +105,53 @@ public class StudentController {
     }
 
     @GetMapping("/purchase/receipt")
-    public void receipt(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model)
-    {
-        model.addAttribute("user",principalDetails.getUser());
-        List<Receipt> receiptList = studentService.findAllUser(principalDetails.getUser());
-        List<Book> bookList = new ArrayList<>();
+    public String receipt(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+        // 현재 사용자 정보를 모델에 추가
+        model.addAttribute("user", principalDetails.getUser());
 
-        for (int i = 0; i < receiptList.size(); i++)
-        {
-            bookList.add(receiptList.get(i).getBook());
+        // 사용자의 구매 내역을 조회
+        List<Receipt> receiptList = studentService.findAllUser(principalDetails.getUser());
+
+        // 만약 구매 내역이 없다면 메시지를 모델에 추가하고 해당 페이지 반환
+        if (receiptList.isEmpty()) {
+            model.addAttribute("noReceipt", "구매 내역이 없습니다.");
+            return "student/purchase/receipt";
         }
 
-        model.addAttribute("bookList",bookList);
-        model.addAttribute("receiveDay",receiptList.get(0).getReceive().getDay());
-        model.addAttribute("receiveCheck",receiptList.get(0).getReceive().getReceiveCheck());
-        model.addAttribute("daySelect",studentService.findReceiveCheck());
+        // 구매 내역이 있다면 책 목록을 생성하여 모델에 추가
+        List<Book> bookList = new ArrayList<>();
+        for (int i = 0; i < receiptList.size(); i++) {
+            bookList.add(receiptList.get(i).getBook());
+        }
+        model.addAttribute("bookList", bookList);
 
+        // 첫 번째 영수증의 수령 날짜와 확인 여부를 모델에 추가
+        model.addAttribute("receiveDay", receiptList.get(0).getReceive().getDay());
+        model.addAttribute("receiveCheck", receiptList.get(0).getReceive().getReceiveCheck());
+
+        // 수령 가능한 일자 목록을 모델에 추가
+        model.addAttribute("daySelect", studentService.findReceiveCheck());
+
+        // 구매 내역이 있는 경우 해당 페이지 반환
+        return "student/purchase/receipt";
     }
 
     @PostMapping("/purchase/receipt")
-    public String postReceipt(@AuthenticationPrincipal PrincipalDetails principalDetails,Model model,@RequestParam String day)
-    {
-        model.addAttribute("user",principalDetails.getUser());
+    public String postReceipt(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model, @RequestParam String day) {
+        // 현재 사용자 정보를 모델에 추가
+        model.addAttribute("user", principalDetails.getUser());
 
-        studentService.updateDay(principalDetails.getUser(),day);
+        // 사용자의 수령 날짜를 업데이트
+        studentService.updateDay(principalDetails.getUser(), day);
 
+        // 날짜를 업데이트한 후 구매 내역이 없다면 메시지를 모델에 추가하고 해당 페이지 반환
+        List<Receipt> updatedReceiptList = studentService.findAllUser(principalDetails.getUser());
+        if (updatedReceiptList.isEmpty()) {
+            model.addAttribute("noReceipt", "구매 내역이 없습니다.");
+            return "student/purchase/receipt";
+        }
+
+        // 구매 내역이 있는 경우 해당 페이지로 리다이렉트
         return "redirect:/student/purchase/receipt";
-
     }
-
 }
