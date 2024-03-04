e97d37fbc748459b092404a9a934f69f0e97bc37
hyunsoo922 <hskang9200@naver.com>
2024-03-04 16:11:22 +0900

    마일리지 경고창

--- LWBS/src/main/resources/static/js/student/book.js
+++ LWBS/src/main/resources/static/js/student/book.js
@@ -9,12 +9,20 @@ $(function(){
         // 배열을 문자열로 변환
         var booksString = selectedBooks.join(',');
 
+        var mileage = $("#mileagePoint");
 
         // hidden input 필드에 설정
         $("#books").val(booksString);
 
 
         // form 제출
-        $("#bookFrm").submit();
+        if(mileage.val() !== null)
+        {
+            $("#bookFrm").submit();
+        }
+        else{
+            alert("사용하실 마일리지를 입력해주세요");
+            mileage.focus();
+        }
     });
 });
