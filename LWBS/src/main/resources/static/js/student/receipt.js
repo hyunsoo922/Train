1fa2c27ebbde1ccd3f7f03efad9a19593094d37b
hyunsoo922 <hskang9200@naver.com>
2024-03-18 14:39:24 +0900

    환불시 결제금액 - 마일리지

--- LWBS/src/main/resources/static/js/student/receipt.js
+++ LWBS/src/main/resources/static/js/student/receipt.js
@@ -5,7 +5,7 @@ $(function(){
             selectedBooks.push($(this).val());
         });
 
-        if(confirm("정말 환불하시겠습니까?"))
+        if(confirm("정말 환불하시겠습니까? ★사용하신 마일리지는 환불되지 않습니다.★"))
         {
             var books = selectedBooks.join(',');
             $.ajax({
