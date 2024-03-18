d1b18ab503775a28ff5c439c27ae8d14b33f9d74
skrheem <skrheem1116@gmail.com>
2024-03-18 15:49:40 +0900

    userid와 bookid에 cascade 걸음

--- LWBS/src/main/java/com/project/LWBS/domain/Cart.java
+++ LWBS/src/main/java/com/project/LWBS/domain/Cart.java
@@ -13,11 +13,11 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "book_id")
     private Book book;
 }
