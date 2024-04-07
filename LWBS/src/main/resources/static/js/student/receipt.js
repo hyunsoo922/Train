153291a3517bfe87ba9fb8e06408c89e80ba2d23
hyunsoo922 <hskang9200@naver.com>
2024-04-08 01:06:37 +0900

    구매내역 수정

--- LWBS/src/main/resources/static/js/student/receipt.js
+++ LWBS/src/main/resources/static/js/student/receipt.js
@@ -1,5 +1,5 @@
 $(function(){
-    $("#cancleBtn").click(function(){
+    $("#cancelBtn").click(function(){
         var book = $(this).val();
 
         if(confirm("정말 환불하시겠습니까? ★사용하신 마일리지는 환불되지 않습니다.★"))
