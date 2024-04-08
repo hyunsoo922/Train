9c92ba84a761e9a57d66c830cdc7449efc3264ec
hyunsoo922 <hskang9200@naver.com>
2024-04-08 20:44:13 +0900

    환불 수정

--- LWBS/src/main/resources/static/js/student/receipt.js
+++ LWBS/src/main/resources/static/js/student/receipt.js
@@ -1,6 +1,7 @@
 $(function(){
-    $("#cancelBtn").click(function(){
+    $(".cancelBtn").click(function(){
         var book = $(this).val();
+        console.log(book)
 
         if(confirm("정말 환불하시겠습니까? ★사용하신 마일리지는 환불되지 않습니다.★"))
         {
