a20dcf5234ee68af8d83bd63bb9864ec8361b4ed
UMTJ <utj1203@gmail.com>
2024-04-15 15:50:14 +0900

    서점페이지 전체 HTML 수정

--- LWBS/src/main/java/com/project/LWBS/controller/BookController.java
+++ LWBS/src/main/java/com/project/LWBS/controller/BookController.java
@@ -2,7 +2,12 @@ package com.project.LWBS.controller;
 
 import com.project.LWBS.config.PrincipalDetails;
 import com.project.LWBS.domain.Book;
+import com.project.LWBS.domain.Department;
+import com.project.LWBS.domain.Receipt;
+import com.project.LWBS.domain.User;
 import com.project.LWBS.service.BookService;
+import com.project.LWBS.service.MyPageService;
+import com.project.LWBS.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,56 +26,51 @@ import java.util.List;
 @Controller
 @RequestMapping("/bookStore")
 public class BookController {
-
-    private final BookService bookService;
+    private static BookService bookService;
+    private static StudentService studentService;
+    private MyPageService mypageService;
 
     @Autowired
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, StudentService studentService, MyPageService mypageService) {
+
         this.bookService = bookService;
+        this.studentService = studentService;
+        this.mypageService = mypageService;
     }
 
     //모든 교재 목록 표시
     @GetMapping("/list")
-    public String getAllBooks(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails,
-                              @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
+    public String getAllBooks(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-        Page<Book> books = bookService.getAllBooks(pageable);
+         // 현재 로그인 중인 유저의 id값을 매개변수로 전달하여 유저가 수강신청한 강의의 교재명 검색 후
+        // 교재명에 맞는 Book 객체들을 리스트로 전달 받음.
+        User user = principalDetails.getUser();
+        int sumMileage = mypageService.sumMileage(user.getId());
+        List<Receipt> receiptList = studentService.findAllUser(principalDetails.getUser());
+        model.addAttribute("user",user);
+        model.addAttribute("mileage", sumMileage);
+        List<Book> bookList = bookService.getAllBook();
+        List<Department> departmentList = bookService.getAllDepartments();
 
-        int nowPage = books.getPageable().getPageNumber() + 1;
-        int startPage = Math.max(nowPage - 4, 1);
-        int endPage = Math.min(nowPage + 7, books.getTotalPages());
+        // Model 객체에 Book 리스트와 User 객체를 담아 View에게 전달
+        model.addAttribute("bookList", bookList);
+        model.addAttribute("departmentList", departmentList);
+        // 첫 번째 영수증의 수령 날짜와 확인 여부를 모델에 추가
+        if (!receiptList.isEmpty()) {
+            model.addAttribute("receiveDay", receiptList.get(0).getReceive().getDay());
+        } else {
+            model.addAttribute("receiveDay", "구매하신 교재가 없습니다."); // 또는 다른 처리를 수행할 수 있음
+        }
+        // 수령 가능한 일자 목록을 모델에 추가
+        model.addAttribute("daySelect", studentService.findReceiveCheck());
 
-        model.addAttribute("user", principalDetails.getUser());
-        model.addAttribute("books", books);
-        model.addAttribute("nowPage", nowPage);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("sortProperty", pageable.getSort().toString());
+        //model.addAttribute("user",principalDetails.getUser());
+        // /home/student 경로를 반환
 
         return "bookStore/list";
     }
 
-    @GetMapping("/listSearch")
-    public String searchByDepartment(@RequestParam("department") String departmentName,
-                                     @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable,
-                                     Model model) {
 
-        Page<Book> searchResults = bookService.findByDepartmentName(departmentName, pageable);
-
-
-        int nowPage = searchResults.getNumber() + 1; // 현재 페이지
-        int startPage = Math.max(nowPage - 2, 1); // 시작 페이지
-        int endPage = Math.min(nowPage + 3, searchResults.getTotalPages()); // 끝 페이지
-
-        // 모델에 데이터 추가
-        model.addAttribute("books", searchResults);
-        model.addAttribute("nowPage", nowPage);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("sortProperty", pageable.getSort().toString());
-
-        return "bookStore/listSearch";
-    }
 
 
 }
